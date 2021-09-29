9e9b9ef4182469fc066a271412b1146b5b722572
GloriaSekyere <gloriasekyere15@gmail.com>
2021-09-29 07:56:01 +0000

    Redux Tutorials 7-17

---
+++ 2. Redux/17. Copy an Object with Object.assign.js
@@ -0,0 +1,25 @@
+const defaultState = {
+    user: 'CamperBot',
+    status: 'offline',
+    friends: '732,982',
+    community: 'freeCodeCamp'
+  };
+  
+  const immutableReducer = (state = defaultState, action) => {
+    switch(action.type) {
+      case 'ONLINE':
+        const newObject = Object.assign({}, state);
+        newObject.status = 'online';
+        return newObject;
+      default:
+        return state;
+    }
+  };
+  
+  const wakeUp = () => {
+    return {
+      type: 'ONLINE'
+    }
+  };
+  
+  const store = Redux.createStore(immutableReducer);
